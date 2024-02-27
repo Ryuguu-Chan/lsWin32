b1b750f3ec4387a69f73a972c89ee09cf3d45e44
Ogan Oezkul <Ogan.Oezkul@ceff.ch>
2024-02-27 14:36:33 +0100

    creation de l'action --author dans Actions.h

--- Actions.h
+++ Actions.h
@@ -4,4 +4,8 @@ namespace Actions {
 	bool SHOW_ALL_DIRECTORIES(const char* str) {
 		return strcmp(str, "-a") || strcmp(str, "--all");
 	}
+
+	bool SHOW_ALL_FILE_AUTHOR(const char* str) {
+		return strcmp(str, "--author");
+	}
 }
