0be511a87a3d867207bd2a2fafce1ab793a1ea2d
Ogan Oezkul <Ogan.Oezkul@ceff.ch>
2024-02-27 13:16:47 +0100

    faire la prise en compte de argv[1]

---
+++ appStrings.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace appStrings {
+	const char* NOT_A_DIRECTORY_ERROR_STR = "ls only works on directories!";
+	const char* DOES_NOT_EXIST_ERROR_STR = "it doesn't exist!";
+}
--- main.cpp
+++ main.cpp
@@ -4,18 +4,37 @@
 #include <filesystem>
 
 #include "Compute.h"
+#include "appStrings.h"
 
 
 namespace filesystem = std::filesystem;
 
 int main(int argc, char** argv) {
 
-	filesystem::path currentWorkingDirectory = filesystem::current_path();
+	filesystem::path currentWorkingDirectory;
 
-	// TODO: prendre en compte les arguments
+	if (argc == 1) {
+		currentWorkingDirectory = filesystem::current_path();
+	}
+	else {
+		// TODO: prendre en compte les options
+		if (filesystem::exists(argv[1])) {
+			if (filesystem::is_directory(argv[1])) {
+				currentWorkingDirectory = argv[1];
+			}
+			else {
+				std::cout << appStrings::NOT_A_DIRECTORY_ERROR_STR << std::endl;
+				return EXIT_FAILURE;
+			}
+		}
+		else {
+			std::cout << appStrings::DOES_NOT_EXIST_ERROR_STR << std::endl;
+			return EXIT_FAILURE;
+		}
+	}
 
 	for (auto const& dir_entry : filesystem::directory_iterator{ currentWorkingDirectory }) {
-		std::cout << dir_entry.path().filename().generic_string() << ", ";
+		std::cout << dir_entry.path().filename().generic_string() << std::endl;
 	}
 
 	std::cout << std::endl;
