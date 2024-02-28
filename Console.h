d5a866215ab51668f16fb2c1908098863f376057
Ogan Oezkul <Ogan.Oezkul@ceff.ch>
2024-02-28 08:56:31 +0100

    creation de la fonction setCursorPosition, getCursorPosition. il faut les tester!!!!

--- Console.h
+++ Console.h
@@ -1,5 +1,34 @@
 #pragma once
 
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
 namespace Console {
 
+#ifdef _WIN32
+	bool setCursorPosition(SHORT x, SHORT y) {
+
+		/*
+		CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+		COORD coordinates;
+		coordinates.X = x;
+		coordinates.Y = y;
+
+		HANDLE consoleOutputHandle = STD_OUTPUT_HANDLE;
+
+		return SetConsoleCursorPosition(consoleOutputHandle, coordinates);
+		*/
+
+		CONSOLE_SCREEN_BUFFER_INFO csbi;
+		csbi.dwCursorPosition.X = x;
+		csbi.dwCursorPosition.Y = y;
+	}
+
+	COORD getCursorPosition() {
+		CONSOLE_SCREEN_BUFFER_INFO csbi;
+		return csbi.dwCursorPosition;
+	}
+#endif
 }
