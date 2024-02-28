9838809293d41c2d4a10d659653f88f7235f499a
Ogan Oezkul <Ogan.Oezkul@ceff.ch>
2024-02-28 08:01:47 +0100

    creation de la methode strQeuals dans Compute.h

--- Compute.h
+++ Compute.h
@@ -18,4 +18,19 @@ namespace Compute {
 
 		return (const char*)out;
 	}
+
+	bool strEquals(const char* str1, const char* str2) {
+
+		if (strlen(str1) != strlen(str2)) {
+			return false;
+		}
+
+		for (int i = 0; i < strlen(str1); i++) {
+			if (str1[i] != str2[i]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
 // RUNS ON C++17 !!!
+// compiler used: Visual Studio C++ compiler
 #include <filesystem>
+
+#include <Windows.h> // WIN32_FILE_ATTRIBUTE_DATA 
+
+#include <string>
+
+// TODO: essayer 
 
 #include "Compute.h"
 #include "appStrings.h"
@@ -30,25 +37,43 @@ int main(int argc, char** argv) {
 		}
 		else {
 			//std::cout << appStrings::DOES_NOT_EXIST_ERROR_STR << std::endl;
+
+			// debug
+			std::cout << argv[1] << std::endl;
 			
 			if (Actions::SHOW_ALL_DIRECTORIES(argv[1])) {
+
+				std::cout << "show everything!!" << std::endl;
 				
 				if (argc == 2) {
 					currentWorkingDirectory = filesystem::current_path();
 				}
-				for (auto const& dir_entry : filesystem::directory_iterator{ currentWorkingDirectory }) {
+				for (auto const& dir_entry : filesystem::directory_iterator { currentWorkingDirectory }) {
 					if (dir_entry.path().filename().generic_string()[0] != '~' && dir_entry.path().filename().generic_string()[0] == '$') {
 						std::cout << dir_entry.path().filename().generic_string() << std::endl;
 					}
 					std::cout << dir_entry.path().filename().generic_string() << std::endl;
 				}
+
+				return EXIT_SUCCESS;
+			}
+			else if (Actions::SHOW_ALL_FILE_AUTHOR(argv[1])) {
+
+				std::cout << "show the authors!!" << std::endl;
+				
+				for (auto const& dir_entry : filesystem::directory_iterator { currentWorkingDirectory }) {
+
+					std::cout << dir_entry.path().filename().generic_string() << '\t';
+					std::cout << "some attributes..." << std::endl;
+				}
+
 				return EXIT_SUCCESS;
 			}
 			else {
 				std::cout << "error";
 			}
 
-			//return EXIT_FAILURE;
+			return EXIT_FAILURE;
 		}
 	}
 
